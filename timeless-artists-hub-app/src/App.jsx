3de0e5dea5874e6dc8e3830bf7ef42214b767ae3
arim83 <jorammercado@pursuit.org>
2023-11-26 09:40:50 -0500

    app.jsx & folder struct.

--- timeless-artists-hub-app/src/App.jsx
+++ timeless-artists-hub-app/src/App.jsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Edit from "./Pages/Edit"
+import EditArtwork from "./Pages/EditArtwork"
+import FourOFour from "./Pages/FourOFour"
+import Home from "./Pages/Home"
+import Index from "./Pages/Index"
+import IndexArtworks from "./Pages/IndexArtworks"
+import New from "./Pages/New"
+import NewArtwork from "./Pages/NewArtwork"
+import Show from "./Pages/Show"
+import ShowArtwork from "./Pages/ShowArtwork"
+import NavBar from "./component/NavBar"
+import IndexArtworksDirect from "./Pages/IndexArtworksDirect"
+import ShowArtworkDirect from "./Pages/ShowArtworkDirect"
+import EditArtworkDirect from "./Pages/EditArtworkDirect"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <Router>
+        <div className="nav">
+          <NavBar />
+        </div>
+        <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artists" element={<Index />} />
+          <Route path="/artworks" element={<IndexArtworksDirect />} />
+          <Route path="/artworks/:id" element={<ShowArtworkDirect />} />
+          <Route path="/artworks/:id/edit" element={<EditArtworkDirect />} />
+          <Route path="/artists/new" element={<New />} />
+          <Route path="/artists/:id" element={<Show />} />
+          <Route path="/artists/:artist_id/artworks" element={<IndexArtworks />} />
+          <Route path="/artists/:artist_id/artworks/new" element={<NewArtwork />} />
+          <Route path="/artists/:artist_id/artworks/:id" element={<ShowArtwork />} />
+          <Route path="/artists/:artist_id/artworks/:id/edit" element={<EditArtwork />} />
+          <Route path="/artists/:id/edit" element={<Edit />} />
+          <Route path="*" element={<FourOFour />} />
+        </Routes>
+        </main>
+      </Router>
+    </div>
   )
 }
 
