06f6b08fc939f043ec3bef8acd9a52254130953b
arim83 <jorammercado@pursuit.org>
2023-11-27 09:15:05 -0500

    artist detail comp

---
+++ timeless-artists-hub-app/src/Components/ArtistDetails.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from "react"
+import { Link, useParams, useNavigate } from "react-router-dom"
+import React from 'react'
+import "./ArtistDetails.css"
+const API = import.meta.env.VITE_BASE_URL
+
+function ArtistDetails() {
+    const [artist, setArtist] = useState({
+        artiste_name: "",
+        birth_year: 0,
+        death_year: 0,
+        genre: "",
+        nationality: "",
+        bio: "",
+        wikipedia_link: "",
+        youtube_link: "",
+        is_favorite: false
+    })
+
+    let navigate = useNavigate()
+    let { artist_id } = useParams()
+
+    useEffect(() => {
+        fetch(`${API}/artistes/${artist_id}`)
+            .then(response => response.json())
+            .then(artist => {
+                setArtist(artist)
+            })
+            .catch(() => navigate("/not-found"))
+    }, [artist_id, navigate])
+
+    const handleDelete = () => {
+        const httpOptions = { "method": "DELETE" }
+        fetch(`${API}/artistes/${artist_id}`, httpOptions)
+            .then((res) => {
+                alert("Artist was deleted!")
+                navigate('/artists')
+            })
+            .catch((err) => console.error(err))
+    }
+
+    return (
+        <article className="topArtist">
+            <table className="tableArtist">
+                <tbody>
+                    <tr className="artistName">
+                        <th colSpan="4"> Name: {artist.artiste_name} </th>
+                    </tr>
+                    <tr className="artistName">
+                        <th colSpan="4"> Bio: {artist.bio} </th>
+                    </tr>
+                    <tr className="artistName">
+                        <th colSpan="4"> Nationality: {artist.nationality} </th>
+                    </tr>
+                    <tr className="artistName">
+                        <th colSpan="4"> Genre: {artist.genre} </th>
+                    </tr>
+                    <tr className="artistName">
+                        <th colSpan="4">
+                            Further Reading:
+                            <Link to={artist.wikipedia_link}> {artist.wikipedia_link}</Link>
+                        </th>
+                    </tr>
+                </tbody>
+                <tbody >
+                    <tr >
+                        <th colSpan="4"> 
+                            <iframe width="420" height="315"
+                                src={`${artist.youtube_link}`}>
+                            </iframe> 
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+            <div className="show-navigation">
+                <Link to={`/artists`}>
+                    <button>Artists</button>
+                </Link>
+                <Link to={`/artists`}>
+                    <button onClick={handleDelete}>Delete</button>
+                </Link>
+                <Link className="back" to={`/artists/`}>Back</Link>
+                <Link className="edit" to={`/artists/${artist_id}/edit`}>Edit</Link>
+                <Link className="artworks" to={`/artists/${artist_id}/artworks`}>Artworks</Link>
+                <Link className="delete" onClick={handleDelete}>Delete</Link>
+            </div>
+            <br></br>
+        </article>
+    )
+}
+
+export default ArtistDetails
